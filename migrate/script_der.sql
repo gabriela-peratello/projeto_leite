0560eda68de369fab22a6ed18f86f1bf4dd2d2a4
gabriela peratello <gabriela.p.silva10@aluno.senai.br>
2025-11-28 11:27:24 -0300

    add: ajustando a organização do banco de dados

---
+++ migrate/script_der.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE registros_leite;
+
+USE registros_leite;
+
+
+CREATE TABLE turma (
+ sala CHAR(10) NOT NULL PRIMARY KEY,
+ representante VARCHAR(50)
+);
+
+CREATE TABLE inventario (
+ id_entrega CHAR(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+ quantidade_caixas INT,
+ dt_entrega DATE,
+ sala CHAR(10)
+);
+
+
+
+ALTER TABLE inventario ADD CONSTRAINT FK_inventario_0 FOREIGN KEY (sala) REFERENCES turma (sala);
+
+
